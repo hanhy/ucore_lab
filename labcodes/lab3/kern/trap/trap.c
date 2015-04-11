dd220f12800941aa141b69e7950cfc219ec134be
onovps <han_hy_thu_12@163.com>
2015-04-11 20:32:09 +0800

    lab3

--- labcodes/lab3/kern/trap/trap.c
+++ labcodes/lab3/kern/trap/trap.c
@@ -36,7 +36,7 @@ static struct pseudodesc idt_pd = {
 /* idt_init - initialize IDT to each of the entry points in kern/trap/vectors.S */
 void
 idt_init(void) {
-     /* LAB1 YOUR CODE : STEP 2 */
+     /* LAB1 2012011276 : STEP 2 */
      /* (1) Where are the entry addrs of each Interrupt Service Routine (ISR)?
       *     All ISR's entry addrs are stored in __vectors. where is uintptr_t __vectors[] ?
       *     __vectors[] is in kern/trap/vector.S which is produced by tools/vector.c
@@ -48,6 +48,19 @@ idt_init(void) {
       *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
       *     Notice: the argument of lidt is idt_pd. try to find it!
       */
+      extern uintptr_t __vectors[];//声明外部变量
+      //所有的中断服务程序都存储在vectors中
+      //__vectors[]出现在kern/trap/vector.S中
+      //在执行了make之后就可以基于tools/vector.c生成vector.S
+      int i;
+      for (i = 0; i < sizeof(idt) / sizeof(struct gatedesc); i ++)
+      	SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
+      
+      SETGATE(idt[T_SWITCH_TOK], 0, GD_KTEXT, __vectors[T_SWITCH_TOK], DPL_USER);
+      //启用中断描述符表中的每一个条目，以便于从用户态转到内核态
+      
+      lidt(&idt_pd);
+      //使用lidt指令告诉CPU中断描述符表的位置
 }
 
 static const char *
@@ -180,12 +193,15 @@ trap_dispatch(struct trapframe *tf) {
     LAB3 : If some page replacement algorithm(such as CLOCK PRA) need tick to change the priority of pages, 
     then you can add code here. 
 #endif
-        /* LAB1 YOUR CODE : STEP 3 */
+        /* LAB1 2012011276 : STEP 3 */
         /* handle the timer interrupt */
         /* (1) After a timer interrupt, you should record this event using a global variable (increase it), such as ticks in kern/driver/clock.c
          * (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
          * (3) Too Simple? Yes, I think so!
          */
+        ticks ++;
+        if (ticks % TICK_NUM == 0)
+            print_ticks();
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
